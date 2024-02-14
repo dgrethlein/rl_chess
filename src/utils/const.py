61910a5330517a5ae7763b058dbb914963ff635e
David Grethlein <12dgrethlein@gmail.com>
2024-02-14 11:11:00 -0500

    Adding a bunch of code block docstrings for the constants of the project.

--- src/utils/const.py
+++ src/utils/const.py
@@ -3,9 +3,45 @@
 """Summary
 
 Attributes:
-    BOARD_COLUMNS (TYPE): Description
-    PIECE_TYPES (TYPE): Description
-    PLAYER_COLORS (TYPE): Description
+    BOARD_COLUMNS (Dict[int,str]): Dictionary linking column indices to letters.
+
+        .. code-block::
+          :name: `BOARD_COLUMNS` code block
+          :linenos:
+          :caption: ``BOARD_COLUMNS``.
+
+          BOARD_COLUMNS = {0 : "a",
+                           1 : "b",
+                           2 : "c",
+                           3 : "d",
+                           4 : "e",
+                           5 : "f",
+                           6 : "g",
+                           7 : "h"}
+
+    PIECE_TYPES (List[str]): List containing all types of chess Piece(s).
+
+        .. code-block::
+          :name: `PIECE_TYPES` code block
+          :linenos:
+          :caption: ``PIECE_TYPES``.
+
+          PIECE_TYPES = ["pawn",
+                         "rook",
+                         "knight",
+                         "bishop",
+                         "queen",
+                         "king"]
+
+    PLAYER_COLORS (List[str]): List containing all Player color(s).
+
+        .. code-block::
+          :name: `PLAYER_COLORS` code block
+          :linenos:
+          :caption: ``PLAYER_COLORS``.
+
+          PLAYER_COLORS = ["black",
+                           "white"]
 """
 
 
